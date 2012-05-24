ef930576541e4a61b77d14ede02dd0c8371e243b
gholt <z-github@brim.net>
2012-05-24 06:18:31 +0000

    Updated LimeChat theme: gholtDecafBland

--- LimeChat Themes/gholtDecafBland.js
+++ LimeChat Themes/gholtDecafBland.js
@@ -2,14 +2,16 @@ function processLine(e) {
     var line = e.target;
     switch (line.getAttribute("type")) {
         case "reply":
+        case "topic":
             var textnode = line.lastChild.firstChild;
-            if (textnode.nodeValue.substr(0, 6) == "Topic:") {
+            if (textnode.nodeValue.substr(1, 5) == "opic:" ||
+                textnode.nodeValue.indexOf("has set topic:") >= 0) {
                 var newdiv = document.createElement("div");
                 newdiv.setAttribute("id", "thetopic");
                 thetopic = line.textContent;
-                trim = thetopic.indexOf("Topic:");
+                trim = thetopic.indexOf("opic:");
                 if (trim > 0) {
-                    thetopic = thetopic.substr(trim + 6);
+                    thetopic = thetopic.substr(trim + 5);
                 }
                 newdiv.innerHTML = thetopic;
                 line.appendChild(newdiv);
