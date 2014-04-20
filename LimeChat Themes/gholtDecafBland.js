adc1e01632bab2832a1094561057f02630c0f4d8
gholt <z-launchpad@brim.net>
2014-04-20 15:19:48 +0000

    Fixed topic element creation.

--- LimeChat Themes/gholtDecafBland.js
+++ LimeChat Themes/gholtDecafBland.js
@@ -6,15 +6,21 @@ function processLine(e) {
             var textnode = line.lastChild.firstChild;
             if (textnode.nodeValue.substr(1, 5) == "opic:" ||
                 textnode.nodeValue.indexOf("has set topic:") >= 0) {
-                var newdiv = document.createElement("div");
-                newdiv.setAttribute("id", "thetopic");
                 thetopic = line.textContent;
                 trim = thetopic.indexOf("opic:");
                 if (trim > 0) {
                     thetopic = thetopic.substr(trim + 5);
                 }
-                newdiv.innerHTML = thetopic.replace(/</g, '&lt;');
-                line.appendChild(newdiv);
+                thetopic = thetopic.replace(/</g, '&lt;');
+                var div = document.getElementById("thetopic")
+                if (div) {
+                    div.innerHTML = thetopic;
+                } else {
+                    div = document.createElement("div");
+                    div.setAttribute("id", "thetopic");
+                    div.innerHTML = thetopic;
+                    line.appendChild(div);
+                }
             }
             break;
     }
